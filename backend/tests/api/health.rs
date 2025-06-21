3404a313b90cbabb36d57c1b6f40ee2b67f324b3
Pavel Mikhalkevich <pavel@mikhalkevich.com>
2025-06-21 19:20:25 +0200

    Add testcontainers Postgres

--- backend/tests/api/health.rs
+++ backend/tests/api/health.rs
@@ -1,19 +1,13 @@
+use crate::utils::setup;
 use rocket::http::{ContentType, Status};
-use rocket::local::asynchronous::Client;
 
 #[rocket::async_test]
 async fn healthz_endpoint() {
     // arrange
-    let rocket = realworld_rocket_react::construct_rocket(
-        "postgres://postgres:postgres@localhost:5432/realworld-rocket-react".into(),
-        true,
-    );
-    let client = Client::tracked(rocket)
-        .await
-        .expect("valid rocket application");
+    let ctx = setup("healthz_endpoint").await;
 
     // act
-    let response = client.get("/healthz").dispatch().await;
+    let response = ctx.client.get("/healthz").dispatch().await;
 
     // assert
     assert_eq!(response.status(), Status::Ok);
---
+++ backend/tests/api/utils.rs
@@ -0,0 +1,36 @@
+use rocket::local::asynchronous::Client;
+use testcontainers_modules::postgres;
+use testcontainers_modules::postgres::Postgres;
+use testcontainers_modules::testcontainers::ContainerAsync;
+use testcontainers_modules::testcontainers::runners::AsyncRunner as _;
+
+pub struct TestContext {
+    #[allow(unused)]
+    pub container: ContainerAsync<Postgres>,
+    pub client: Client,
+}
+
+pub(crate) async fn setup(test_name: &'static str) -> TestContext {
+    // arrange
+    let container = postgres::Postgres::default()
+        .with_db_name(test_name)
+        .start()
+        .await
+        .expect("successfully launched PostgreSQL container");
+    let host_port = container
+        .get_host_port_ipv4(5432)
+        .await
+        .expect("port to has been assigned on the host");
+    let database_url = format!(
+        "postgres://postgres:postgres@localhost:{}/{}",
+        host_port, test_name
+    );
+    let rocket = realworld_rocket_react::construct_rocket(
+        database_url,
+        true, // _do_ run migrations
+    );
+    let client = Client::tracked(rocket)
+        .await
+        .expect("valid rocket application");
+    TestContext { container, client }
+}
