c2355f4cd66d376dcdc9790a1b1134c749ac881e
Pavel Mikhalkevich <pavel@mikhalkevich.com>
2025-07-06 16:52:02 +0200

    Fix eslint FlatConfig deprecation warning

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,7 +1,7 @@
 import tseslint from "typescript-eslint";
 import baseEslintConfig from "./configs/eslint/eslint.config.base.mjs";
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
+/** @type {import('eslint').Linter.Config[]} */
 export default tseslint.config(...baseEslintConfig, {
   rules: {
     "@typescript-eslint/ban-ts-comment": "warn",
