bd4029e4e3b47db720eec74274750ecbc2e6b40a
Pavel Mikhalkevich <pavel@mikhalkevich.com>
2025-07-12 16:40:38 +0200

    Serve openapi as JSON

--- backend/src/openapi.rs
+++ backend/src/openapi.rs
@@ -1,8 +1,17 @@
+use std::sync::LazyLock;
+
 use crate::http::guards::SecurityAddon;
 use rocket::fairing::AdHoc;
 use utoipa::OpenApi;
 use utoipa_scalar::Scalar;
 use utoipa_scalar::Servable;
+
+const OPENAPI_JSON: LazyLock<&'static str> = LazyLock::new(|| {
+    ApiDoc::openapi()
+        .to_pretty_json()
+        .expect("valid json")
+        .leak()
+});
 
 #[derive(OpenApi)]
 #[openapi(
@@ -13,8 +22,17 @@ use utoipa_scalar::Servable;
     )]
 struct ApiDoc;
 
+#[get("/openapi.json")]
+fn openapi() -> &'static str {
+    &*OPENAPI_JSON
+}
+
 pub(crate) fn stage() -> AdHoc {
-    AdHoc::on_ignite("Openapi3 Scalar UI Stage", move |rocket| async move {
-        rocket.mount("/", Scalar::with_url("/scalar", ApiDoc::openapi()))
+    AdHoc::on_ignite("Openapi JSON & Scalar UI Stage", move |rocket| async move {
+        // generate pretty-formatted openapi spec, on app's startup
+        let _open_api = &*OPENAPI_JSON;
+        rocket
+            .mount("/", Scalar::with_url("/scalar", ApiDoc::openapi()))
+            .mount("/", routes![openapi])
     })
 }
--- frontend/src/api/generated.ts
+++ frontend/src/api/generated.ts
@@ -2,14 +2,18 @@ import { base as api } from "./base";
 
 const injectedRtkApi = api.injectEndpoints({
   endpoints: (build) => ({
-    readCurrentUser: build.query<ReadCurrentUserApiResponse, ReadCurrentUserApiArg>({
-      query: () => ({ url: `/api/user/` }),
+    read: build.query<ReadApiResponse, ReadApiArg>({
+      query: () => ({ url: `/api/user` }),
     }),
-    updateCurrentUser: build.mutation<UpdateCurrentUserApiResponse, UpdateCurrentUserApiArg>({
-      query: () => ({ url: `/api/user/`, method: "PUT" }),
+    update: build.mutation<UpdateApiResponse, UpdateApiArg>({
+      query: () => ({ url: `/api/user`, method: "PUT" }),
     }),
-    createUser: build.mutation<CreateUserApiResponse, CreateUserApiArg>({
-      query: () => ({ url: `/api/user/`, method: "POST" }),
+    handler: build.mutation<HandlerApiResponse, HandlerApiArg>({
+      query: (queryArg) => ({
+        url: `/api/user`,
+        method: "POST",
+        body: queryArg.userPayloadRegistration,
+      }),
     }),
     login: build.mutation<LoginApiResponse, LoginApiArg>({
       query: () => ({ url: `/api/user/login`, method: "POST" }),
@@ -18,12 +22,53 @@ const injectedRtkApi = api.injectEndpoints({
   overrideExisting: false,
 });
 export { injectedRtkApi as api };
-export type ReadCurrentUserApiResponse = unknown;
-export type ReadCurrentUserApiArg = void;
-export type UpdateCurrentUserApiResponse = unknown;
-export type UpdateCurrentUserApiArg = void;
-export type CreateUserApiResponse = unknown;
-export type CreateUserApiArg = void;
+export type ReadApiResponse = unknown;
+export type ReadApiArg = void;
+export type UpdateApiResponse = unknown;
+export type UpdateApiArg = void;
+export type HandlerApiResponse = /** status 201 User successfully created */ UserPayloadUser;
+export type HandlerApiArg = {
+  userPayloadRegistration: UserPayloadRegistration;
+};
 export type LoginApiResponse = unknown;
 export type LoginApiArg = void;
-export const { useReadCurrentUserQuery, useUpdateCurrentUserMutation, useCreateUserMutation, useLoginMutation } = injectedRtkApi;
+export type UserPayloadUser = {
+  user: {
+    /** User's biography.
+        
+        Empty string means biography has never been provided. */
+    bio: string;
+    /** User's email, e.g. `rob.pike@gmail.com`. */
+    email: string;
+    /** Location of user's image (if any). */
+    image?: string | null;
+    /** Fresh JWT token. */
+    token: string;
+    /** User's name or nickname.
+        
+        This is  - just like [`User::email`] - unique in the system. */
+    username: string;
+  };
+};
+export type Validation = {
+  errors: {
+    [key: string]: string[];
+  };
+};
+export type UserPayloadRegistration = {
+  user: {
+    /** User's email, e.g. `rob.pike@gmail.com`.
+        
+        This is case-insensitively unique in the system. */
+    email: string;
+    /** User's password.
+        
+        There are currently no limitations on password strength. */
+    password: string;
+    /** User's name or nickname.
+        
+        This is  - just like [`User::email`] - unique in the system. */
+    username: string;
+  };
+};
+export const { useReadQuery, useUpdateMutation, useHandlerMutation, useLoginMutation } = injectedRtkApi;
