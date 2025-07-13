c3f25fef4ff4bb999fab288597dcb2954688ebbe
Pavel Mikhalkevich <pavel@mikhalkevich.com>
2025-07-13 13:10:12 +0200

    Restore FE dir

--- frontend/src/api/generated.ts
+++ frontend/src/api/generated.ts
@@ -2,18 +2,14 @@ import { base as api } from "./base";
 
 const injectedRtkApi = api.injectEndpoints({
   endpoints: (build) => ({
-    read: build.query<ReadApiResponse, ReadApiArg>({
-      query: () => ({ url: `/api/user` }),
+    readCurrentUser: build.query<ReadCurrentUserApiResponse, ReadCurrentUserApiArg>({
+      query: () => ({ url: `/api/user/` }),
     }),
-    update: build.mutation<UpdateApiResponse, UpdateApiArg>({
-      query: () => ({ url: `/api/user`, method: "PUT" }),
+    updateCurrentUser: build.mutation<UpdateCurrentUserApiResponse, UpdateCurrentUserApiArg>({
+      query: () => ({ url: `/api/user/`, method: "PUT" }),
     }),
-    handler: build.mutation<HandlerApiResponse, HandlerApiArg>({
-      query: (queryArg) => ({
-        url: `/api/user`,
-        method: "POST",
-        body: queryArg.userPayloadRegistration,
-      }),
+    createUser: build.mutation<CreateUserApiResponse, CreateUserApiArg>({
+      query: () => ({ url: `/api/user/`, method: "POST" }),
     }),
     login: build.mutation<LoginApiResponse, LoginApiArg>({
       query: () => ({ url: `/api/user/login`, method: "POST" }),
@@ -22,53 +18,12 @@ const injectedRtkApi = api.injectEndpoints({
   overrideExisting: false,
 });
 export { injectedRtkApi as api };
-export type ReadApiResponse = unknown;
-export type ReadApiArg = void;
-export type UpdateApiResponse = unknown;
-export type UpdateApiArg = void;
-export type HandlerApiResponse = /** status 201 User successfully created */ UserPayloadUser;
-export type HandlerApiArg = {
-  userPayloadRegistration: UserPayloadRegistration;
-};
+export type ReadCurrentUserApiResponse = unknown;
+export type ReadCurrentUserApiArg = void;
+export type UpdateCurrentUserApiResponse = unknown;
+export type UpdateCurrentUserApiArg = void;
+export type CreateUserApiResponse = unknown;
+export type CreateUserApiArg = void;
 export type LoginApiResponse = unknown;
 export type LoginApiArg = void;
-export type UserPayloadUser = {
-  user: {
-    /** User's biography.
-        
-        Empty string means biography has never been provided. */
-    bio: string;
-    /** User's email, e.g. `rob.pike@gmail.com`. */
-    email: string;
-    /** Location of user's image (if any). */
-    image?: string | null;
-    /** Fresh JWT token. */
-    token: string;
-    /** User's name or nickname.
-        
-        This is  - just like [`User::email`] - unique in the system. */
-    username: string;
-  };
-};
-export type Validation = {
-  errors: {
-    [key: string]: string[];
-  };
-};
-export type UserPayloadRegistration = {
-  user: {
-    /** User's email, e.g. `rob.pike@gmail.com`.
-        
-        This is case-insensitively unique in the system. */
-    email: string;
-    /** User's password.
-        
-        There are currently no limitations on password strength. */
-    password: string;
-    /** User's name or nickname.
-        
-        This is  - just like [`User::email`] - unique in the system. */
-    username: string;
-  };
-};
-export const { useReadQuery, useUpdateMutation, useHandlerMutation, useLoginMutation } = injectedRtkApi;
+export const { useReadCurrentUserQuery, useUpdateCurrentUserMutation, useCreateUserMutation, useLoginMutation } = injectedRtkApi;
