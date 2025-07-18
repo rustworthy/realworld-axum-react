61bf83423ca700447dd6c2cd18a845e36dd1e067
Pavel Mikhalkevich <pavel@mikhalkevich.com>
2025-07-18 22:21:15 +0200

    Regenerate hooks

--- frontend/src/api/generated.ts
+++ frontend/src/api/generated.ts
@@ -3,27 +3,112 @@ import { base as api } from "./base";
 const injectedRtkApi = api.injectEndpoints({
   endpoints: (build) => ({
     readCurrentUser: build.query<ReadCurrentUserApiResponse, ReadCurrentUserApiArg>({
-      query: () => ({ url: `/api/user/` }),
+      query: () => ({ url: `/api/user` }),
     }),
     updateCurrentUser: build.mutation<UpdateCurrentUserApiResponse, UpdateCurrentUserApiArg>({
-      query: () => ({ url: `/api/user/`, method: "PUT" }),
+      query: (queryArg) => ({
+        url: `/api/user`,
+        method: "PUT",
+        body: queryArg.userPayloadUserUpdate,
+      }),
     }),
-    createUser: build.mutation<CreateUserApiResponse, CreateUserApiArg>({
-      query: () => ({ url: `/api/user/`, method: "POST" }),
+    registerUser: build.mutation<RegisterUserApiResponse, RegisterUserApiArg>({
+      query: (queryArg) => ({
+        url: `/api/user`,
+        method: "POST",
+        body: queryArg.userPayloadRegistration,
+      }),
     }),
     login: build.mutation<LoginApiResponse, LoginApiArg>({
-      query: () => ({ url: `/api/user/login`, method: "POST" }),
+      query: (queryArg) => ({
+        url: `/api/user/login`,
+        method: "POST",
+        body: queryArg.userPayloadLogin,
+      }),
     }),
   }),
   overrideExisting: false,
 });
 export { injectedRtkApi as api };
-export type ReadCurrentUserApiResponse = unknown;
+export type ReadCurrentUserApiResponse = /** status 200 User details and fresh JWT. */ UserPayloadUser;
 export type ReadCurrentUserApiArg = void;
-export type UpdateCurrentUserApiResponse = unknown;
-export type UpdateCurrentUserApiArg = void;
-export type CreateUserApiResponse = unknown;
-export type CreateUserApiArg = void;
-export type LoginApiResponse = unknown;
-export type LoginApiArg = void;
-export const { useReadCurrentUserQuery, useUpdateCurrentUserMutation, useCreateUserMutation, useLoginMutation } = injectedRtkApi;
+export type UpdateCurrentUserApiResponse = /** status 200 User details and fresh JWT. */ UserPayloadUser;
+export type UpdateCurrentUserApiArg = {
+  userPayloadUserUpdate: UserPayloadUserUpdate;
+};
+export type RegisterUserApiResponse = /** status 201 User successfully created */ UserPayloadUser;
+export type RegisterUserApiArg = {
+  userPayloadRegistration: UserPayloadRegistration;
+};
+export type LoginApiResponse = /** status 200 User successfully logged in */ UserPayloadUser;
+export type LoginApiArg = {
+  userPayloadLogin: UserPayloadLogin;
+};
+export type UserPayloadUser = {
+  user: {
+    /** User's biography.
+        
+        Empty string means biography has never been provided. */
+    bio: string;
+    /** User's email, e.g. `rob.pike@gmail.com`. */
+    email: string;
+    /** Location of user's image (if any). */
+    image?: string | null;
+    /** Fresh JWT token. */
+    token: string;
+    /** User's name or nickname.
+        
+        This is  - just like the user's `email` - case-insensitively unique
+        in the system. */
+    username: string;
+  };
+};
+export type Validation = {
+  errors: {
+    [key: string]: string[];
+  };
+};
+export type UserPayloadUserUpdate = {
+  user: {
+    /** User's biography.
+        
+        Empty string means biography has never been provided. */
+    bio?: string | null;
+    /** User's email, e.g. `rob.pike@gmail.com`. */
+    email?: string | null;
+    /** User's name or nickname.
+        
+        This is  - just like the user's `email` - case-insensitively unique
+        in the system. */
+    username?: string | null;
+  };
+};
+export type UserPayloadRegistration = {
+  user: {
+    /** User's email, e.g. `rob.pike@gmail.com`.
+        
+        This is case-insensitively unique in the system. */
+    email: string;
+    /** User's password.
+        
+        There are currently no limitations on password strength. */
+    password: string;
+    /** User's name or nickname.
+        
+        This is  - just like the user's `email` - case-insensitively unique
+        in the system. */
+    username: string;
+  };
+};
+export type UserPayloadLogin = {
+  user: {
+    /** User's email, e.g. `rob.pike@gmail.com`.
+        
+        This is case-insensitively unique in the system. */
+    email: string;
+    /** User's password. */
+    password: string;
+  };
+};
+export const { useReadCurrentUserQuery, useUpdateCurrentUserMutation, useRegisterUserMutation, useLoginMutation } =
+  injectedRtkApi;
