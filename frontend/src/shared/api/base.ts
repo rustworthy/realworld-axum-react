74e80107cc5c6f1ea72c74c2b01c17b3eb6dfe06
Pavel Mikhalkevich <pavel@mikhalkevich.com>
2025-08-05 18:15:19 +0200

    Set bearer if user authenticated

--- frontend/src/shared/api/base.ts
+++ frontend/src/shared/api/base.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 import { config } from "@/config";
+import { AppState } from "declarations";
 
 export type Article = {
   id: number;
@@ -14,12 +15,9 @@ export const base = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: config.BACKEND_URL,
     prepareHeaders: (headers, { getState }) => {
-      // effectively no-op for now, but will be updated with the JWT,
-      // once the hook is in place
-      // @ts-expect-error - it's unknown for the time being
-      const token = getState().auth?.user?.token;
-      if (token) {
-        headers.set("Authorization", `Bearer ${token}`);
+      const state = getState() as AppState;
+      if (state.auth.isAuthenticated) {
+        headers.set("Authorization", `Bearer ${state.auth.user!.token}`);
       }
       return headers;
     },
