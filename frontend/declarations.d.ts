b75e2868cfb44221a52c85c06b81342dddc7a0bb
Pavel Mikhalkevich <pavel@mikhalkevich.com>
2025-08-08 21:48:54 +0200

    Restore theme type import in declarations

--- frontend/declarations.d.ts
+++ frontend/declarations.d.ts
@@ -1,3 +1,5 @@
+import { AppThemeType } from "./src/shared/providers/ThemeProvider";
+
 declare module "@emotion/react" {
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface Theme extends AppThemeType {}
