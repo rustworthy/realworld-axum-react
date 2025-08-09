71f47fb3b12d995ec5bf15727557df8f8e53c539
Pavel Mikhalkevich <pavel@mikhalkevich.com>
2025-08-09 22:41:27 +0200

    Use numeric string for OTP

--- frontend/src/pages/ConfirmEmailPage/ui/ConfirmEmailPage.schema.ts
+++ frontend/src/pages/ConfirmEmailPage/ui/ConfirmEmailPage.schema.ts
@@ -1,15 +1,15 @@
 import * as z from "zod";
 
-const ALPHANUMERIC = /^[a-z0-9]+$/i;
+const NUMERIC = /^[0-9]+$/i;
 
 export const OTP_LENGTH = 8;
 
 export const confirmEmailSchema = z.object({
   otp: z
     .string()
-    .length(OTP_LENGTH, { error: `Should contain ${OTP_LENGTH} characters.` })
-    .refine((value) => ALPHANUMERIC.test(value), {
-      error: "Should only contain letters and numbers.",
+    .length(OTP_LENGTH, { error: "Invalid OTP length." })
+    .refine((value) => NUMERIC.test(value), {
+      error: "Should only contain numbers.",
     }),
 });
 
