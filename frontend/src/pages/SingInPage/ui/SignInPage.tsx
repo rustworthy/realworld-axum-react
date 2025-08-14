2ebe2a5638f8512485366e665a68699d6f564d21
Pavel Mikhalkevich <pavel@mikhalkevich.com>
2025-08-14 22:04:18 +0200

    Add anonym routes group

--- frontend/src/pages/SingInPage/ui/SignInPage.tsx
+++ frontend/src/pages/SingInPage/ui/SignInPage.tsx
@@ -1,14 +1,5 @@
-import { Navigate } from "react-router";
-
-import { useAuth } from "@/shared/auth";
-import { ROUTES } from "@/shared/constants/routes.constants";
-
 import * as S from "./SignInPage.styles";
 
 export const SignInPage = () => {
-  const { isAuthenticated } = useAuth();
-
-  if (isAuthenticated) return <Navigate to={ROUTES.HOME} />;
-
   return <S.Title> SignInPage</S.Title>;
 };
