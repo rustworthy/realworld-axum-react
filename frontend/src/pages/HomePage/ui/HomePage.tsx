487e3f63f2f58aa15375145e3aae15d75b431639
Pavel Mikhalkevich <pavel@mikhalkevich.com>
2025-09-26 21:19:42 +0200

    Basic pagination logic

--- frontend/src/pages/HomePage/ui/HomePage.tsx
+++ frontend/src/pages/HomePage/ui/HomePage.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import { useSearchParams } from "react-router";
 
 import { useAuth } from "@/features/auth";
@@ -9,7 +9,8 @@ import { LayoutContainer } from "@/shared/ui/Container";
 
 import * as S from "./HomePage.styles";
 
-const IS_PAGINATION_FEATURE_FINISHED = false;
+
+const ARTICLES_PER_PAGE = 4;
 
 export type FeedType = "personal" | "global";
 
@@ -19,10 +20,27 @@ export type FeedSearchParams = {
 
 export const HomePage: FC = () => {
   const { isAuthenticated } = useAuth();
-  const [searchParams, _setSearchParams] = useSearchParams({ feed: "global" });
+
+  const [searchParams, setSearchParams] = useSearchParams({ feed: "global", page: "1" });
   const isPersonalFeed = searchParams.get("feed") === "personal";
-  const { data, isLoading } = useListArticlesQuery({ limit: 10 });
-  const empty = !isLoading && (!data || data.articlesCount === 0);
+  const parsedPage = parseInt(searchParams.get("page")!);
+  const page = isNaN(parsedPage) || parsedPage < 1 ? 1 : parsedPage;
+  const offset = (page - 1) * ARTICLES_PER_PAGE;
+  const { data, isLoading } = useListArticlesQuery({ limit: ARTICLES_PER_PAGE, offset });
+  const pagesCount = useMemo(() => data ? Math.ceil(data.articlesCount / ARTICLES_PER_PAGE) : null, [data]);
+  // it's possible that url contains page that is past the aricles: e.g. they might
+  // have manullay inserted the parameter (which is less likely) or the number of
+  // articles decreased prior to them refreshing the page, and so there _are_ articles,
+  // but not at thos offset; so we need to check both `artcilesCount` and `articles.length`
+  const empty = !isLoading && (!data || data.articlesCount === 0 || data.articles.length === 0);
+  const shouldPaginate = typeof pagesCount === "number" && pagesCount > 0 && !empty;
+
+  const onPageButtonClick = (page: number) => {
+    setSearchParams((params) => {
+      params.set("page", page.toString());
+      return params;
+    });
+  }
 
   return (
     <S.PageWrapper>
@@ -54,22 +72,21 @@ export const HomePage: FC = () => {
               ? null
               : isLoading
                 ? // TODO: add skeleton while loading
-                  null
+                null
                 : data!.articles.map((article) => (
-                    <Preview actionsEnabled={isAuthenticated} article={article} key={article.slug} />
-                  ))}
-            {IS_PAGINATION_FEATURE_FINISHED ? (
+                  <Preview actionsEnabled={isAuthenticated} article={article} key={article.slug} />
+                ))}
+            {shouldPaginate ? (
               <ul className="pagination">
-                <li className="page-item active">
-                  <a className="page-link" href="">
-                    1
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="">
-                    2
-                  </a>
-                </li>
+                {
+                  Array(pagesCount).keys().map(idx => (
+                    <li key={idx} className="page-item active">
+                      <button onClick={() => onPageButtonClick(idx + 1)}>
+                        {idx + 1}
+                      </button>
+                    </li>
+                  ))
+                }
               </ul>
             ) : null}
           </S.FeedContainer>
@@ -79,6 +96,6 @@ export const HomePage: FC = () => {
           </S.TagsContainer>
         </S.MainContent>
       </LayoutContainer>
-    </S.PageWrapper>
+    </S.PageWrapper >
   );
 };
