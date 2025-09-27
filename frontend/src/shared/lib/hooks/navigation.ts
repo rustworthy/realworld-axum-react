b90e777cac905b2d82c9e31707013e163474c9d3
Pavel Mikhalkevich <pavel@mikhalkevich.com>
2025-09-27 14:15:53 +0200

    Fix deps arrays and typos

--- frontend/src/shared/lib/hooks/navigation.ts
+++ frontend/src/shared/lib/hooks/navigation.ts
@@ -6,9 +6,10 @@ export function useHashScrollIn(awaitedResource?: unknown) {
   const location = useLocation();
 
   useEffect(() => {
-    // only scroll if that's a first load with resource already loaded/fetched:
+    // only scroll if that's the first load with resource _already_ loaded/fetched:
     // we do not want to scroll again when they mutate the data, e.g. favorite
-    // or unfavorite this article;
+    // or unfavorite the article they have loaded with the link someone shared
+    // with them;
     if (scrolled.current) return;
     const id = location.hash.slice(1);
     if (id) {
@@ -18,8 +19,8 @@ export function useHashScrollIn(awaitedResource?: unknown) {
         setTimeout(() => element.scrollIntoView({ behavior: "smooth", block: "start" }), 500);
       }
     }
-    // initally, the data (e.g. article) may not be in the cache and
-    // so we want to check again once the resource is fully loaded (e.g. the
+    // initially, the data (e.g. article) may not be in the cache and so we want
+    // to scan the document again once the resource is fully loaded (e.g. the
     // artcile's body is there and may contain the hash we are looking for)
-  }, [awaitedResource, location.hash]);
+  }, [awaitedResource]);
 }
