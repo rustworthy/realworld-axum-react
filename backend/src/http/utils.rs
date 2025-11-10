834f95cd50fd2870352bf79ac74fc79474b59ff0
Pavel Mikhalkevich <pavel@mikhalkevich.com>
2025-11-10 20:19:20 +0100

    Cache verdict

--- backend/src/http/utils.rs
+++ backend/src/http/utils.rs
@@ -1,5 +1,9 @@
 use crate::AppContext;
 use crate::http::errors::Error;
+use crate::services::moderator::Verdict;
+use crate::utils;
+use anyhow::Context as _;
+use deadpool_redis::redis::Cmd as RedisCmd;
 
 pub async fn moderate_content(ctx: &AppContext, content: &str, field: &str) -> Result<(), Error> {
     if ctx.skip_content_moderation {
@@ -8,6 +12,8 @@ pub async fn moderate_content(ctx: &AppContext, content: &str, field: &str) -> R
     }
 
     let verdict = ctx.moderator.moderate(content).await?;
+    cache_verdict(content, &verdict, ctx).await?;
+
     if !verdict.processable {
         return Err(Error::unprocessable_entity([(
             field,
@@ -29,6 +35,30 @@ pub async fn moderate_content(ctx: &AppContext, content: &str, field: &str) -> R
             indecent content in text and/or images"#,
         )]));
     }
+
+    Ok(())
+}
+
+pub async fn cache_verdict(
+    content: &str,
+    verdict: &Verdict,
+    ctx: &AppContext,
+) -> anyhow::Result<()> {
+    let mut conn = ctx
+        .redis
+        .get()
+        .await
+        .context("failed to acquire Redis connenction from pool")?;
+    let content_key = format!("moderation::{}", utils::md5_hash(content));
+    let verdict_string = serde_json::to_string(&verdict).context("failed to serialize verdict")?;
+    RedisCmd::set_ex(
+        content_key,
+        verdict_string,
+        60 * 60 * 24 * 30, // for a month
+    )
+    .exec_async(&mut conn)
+    .await
+    .context("failed to cache verdict")?;
 
     Ok(())
 }
